b0b6e0b6d6ec67ac3976c463987b98b28fd560ae
Harsh Pandey <harshp6421@gmail.com>
2023-04-19 07:30:05 +0530

    Final quiz project

---
+++ main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank=[]
+for i in range (len(question_data)):
+    """Appending text and answer"""
+    question_bank.append(Question(question_data[i]["text"],question_data[i]["answer"]))
+
+quiz=QuizBrain(question_bank)
+quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
