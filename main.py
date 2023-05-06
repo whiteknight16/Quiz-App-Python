68682e059925c6e4423a49143dc5c575e26face2
Harsh Pandey <harshp6421@gmail.com>
2023-05-06 21:13:51 +0530

    Added UI Features

--- main.py
+++ main.py
@@ -8,11 +8,4 @@ for i in range (len(question_data)):
     question_bank.append(Question(question_data[i]["text"],question_data[i]["answer"]))
 
 quiz=QuizBrain(question_bank)
-# quiz.next_question()
 quiz_ui=QuizInterface(quiz)
-
-# while quiz.still_has_questions():
-#     quiz.next_question()
-
-# print("You have completed the quiz")
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
